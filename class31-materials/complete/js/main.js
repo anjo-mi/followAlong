f709d6b94740cc7c0266e4cd697a0991980d6f28
kyle <nojaim.k@gmail.com>
2024-08-13 16:59:19 -0400

    redid farm classes and subclasses

--- class31-materials/complete/js/main.js
+++ class31-materials/complete/js/main.js
@@ -1,3 +1,47 @@
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+
+//     get name(){
+//         return this._name
+//     }
+
+//     speak(){
+//         console.log(`${this._name} makes a sounds`)
+//     }
+// }
+
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+
+//     speak(){
+//         super.speak()
+//         console.log(`${this._name} barks`)
+//     }
+// }
+
+// class Cat extends Animal{
+//     constructor(name, breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this._name} purrs`)
+//     }
+// }
+
+
 class Animal{
     constructor(name){
         this._name = name
