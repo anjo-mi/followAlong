19914d39d46a917884b8f7900e42199704313fee
kyle <nojaim.k@gmail.com>
2024-12-06 22:06:21 -0500

    ending at start of mongo

--- crud-app/server.js
+++ crud-app/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({extended: true}))
+
 app.listen(3000, (req,res) => {
     console.log('express yourself')
 })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
 console.log('node way')
