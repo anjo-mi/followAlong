e54e7a0aaee8bb33cb90f9e064794c92eb7534fd
kyle <nojaim.k@gmail.com>
2025-01-04 15:26:13 -0500

    holy shit that was too much effort, but one sprite is working lol

---
+++ crud-run/js/main.js
@@ -0,0 +1,25 @@
+function cycleSprites(){
+    const sprite = document.getElementById('brook-sprite');
+    let currentIndex = 0;
+    const spriteStates = [
+        {class: 'sprite-one', position: '-3px -112px'},
+        {class: 'sprite-two', position: '-152px -120px'},
+        {class: 'sprite-three', position: '-897px -112px'},
+        {class: 'sprite-four', position: '-1010px -103px'},
+    ];
+
+    setInterval(() => {
+        sprite.style.opacity = '0';
+
+        setTimeout(() => {
+
+            sprite.className = spriteStates[currentIndex].class;
+            sprite.style.backgroundPosition = spriteStates[currentIndex].position;
+            sprite.style.opacity = '1';
+
+            currentIndex = (currentIndex + 1) % spriteStates.length;
+        },150);
+    }, 700)
+}
+
+document.addEventListener('DOMContentLoaded', cycleSprites);
