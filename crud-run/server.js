d85c114f86f1a03e43b3df1c0305fc83ea600b3c
kyle <nojaim.k@gmail.com>
2025-01-04 20:49:05 -0500

    need to find jimbei

--- crud-run/server.js
+++ crud-run/server.js
@@ -25,36 +25,60 @@ const luffy = new Theme('Monkey D. Luffy',
                         '/images/luffy.webp', 
                         ['determination', 'loyalty', 'rubber-man'], 
                         ['aloofness', 'lack of intelligence', 'lack of discipline'], 
-                        'sprites/luffy.png', 
-                        'gum-gum-anything').addTheme();
+                        'sprites/luffy-trans.png', 
+                        'gum-gum-anything',
+                        [
+                            {class: 'sprite-one', position: '-307px -542px', height: '54px', width: '48px'},
+                            {class: 'sprite-two', position: '-160px -3333px', height: '80px', width: '157px'},
+                            {class: 'sprite-three', position: '-478px -3333px', height: '80px', width: '100px'},
+                            {class: 'sprite-four', position: '-205px -3454px', height: '80px', width: '187px'}
+                        ]).addTheme();
 
 const zoro = new Theme('Roronoa Zoro', 
                         '/images/zoro.jpg', 
                         ['determination', 'loyalty', 'three-sword-style'], 
                         ['sense of direction', 'lack of intelligence'], 
-                        'sprites/zoro.png', 
-                        'Wado Ichimonji, Sandai Kitetsu, and Enma').addTheme();
+                        'sprites/zoro-trans.png', 
+                        'Wado Ichimonji, Sandai Kitetsu, and Enma',
+                        [
+                            {class: 'sprite-one', position: '-636px -200px', height: '63px', width: '41px'},
+                            {class: 'sprite-two', position: '-139px -192px', height: '71px', width: '49px'},
+                            {class: 'sprite-three', position: '-198px -196px', height: '67px', width: '85px'},
+                            {class: 'sprite-four', position: '-293px -220px', height: '43px', width: '86px'}
+                        ]).addTheme();
         
 const sanji = new Theme('Vinsmoke Sanji', 
                         '/images/sanji.jpg', 
                         ['chivalry', 'cooking', 'kicking'], 
                         ['familial issues', 'any attractive woman'], 
-                        'sprites/sanji.png', 
-                        'never hands, always feet').addTheme();
+                        'sprites/sanji-trans.png', 
+                        'never hands, always feet', 
+                        [
+                            {class: 'sprite-one', position: '-352px -315px', height: '62px', width: '35px'},
+                            {class: 'sprite-two', position: '-120px -307px', height: '69px', width: '62px'},
+                            {class: 'sprite-three', position: '-187px -310px', height: '66px', width: '61px'},
+                            {class: 'sprite-four', position: '-317px -314px', height: '62px', width: '30px'}
+                        ]).addTheme();
 
 const usopp = new Theme('God King Usopp', 
                         '/images/usopp.jpg', 
                         ['sharp-shooter', 'quick-thinking', 'hard-working'], 
                         ['cowardice', 'brazenness'], 
-                        'sprites/usopp.png', 
-                        'Slingshot and Kabuto').addTheme();
+                        'sprites/usopp-trans.png', 
+                        'Slingshot and Kabuto',
+                        [
+                            {class: 'sprite-one', position: '-98px -1129px', height: '57px', width: '53px'},
+                            {class: 'sprite-two', position: '-55px 1128px', height: '58px', width: '42px'},
+                            {class: 'sprite-three', position: '-214px -1106px', height: '80px', width: '69px'},
+                            {class: 'sprite-four', position: '-284px -1108px', height: '78px', width: '69px'}
+                        ]).addTheme();
 
 const robin = new Theme('Nico Robin', 
                         '/images/robin.jpg', 
                         ['determination', 'survivor', 'knowledge of ancient history'], 
                         ['feels undeserving', 'long-wanted by the marines'], 
                         'sprites/robin-trans.png', 
-                        'Flower Flower Fruit'
+                        'Flower Flower Fruit',
                         [
                             {class: 'sprite-one', position: '-14px -964px', height: '65px', width: '36px'},
                             {class: 'sprite-two', position: '-119px -951px', height: '78px', width: '39px'},
@@ -67,7 +91,7 @@ const nami = new Theme('Nami',
                         ['survivor', 'eye for profit', 'navigation'], 
                         ['greed', 'self-interest'], 
                         'sprites/nami-trans.png', 
-                        'Clima-Tact'
+                        'Clima-Tact',
                         [
                             {class: 'sprite-one', position: '-529px -102px', height: '70px', width: '37px'},
                             {class: 'sprite-two', position: '-140px -87px', height: '85px', width: '105px'},
@@ -93,7 +117,7 @@ const franky = new Theme('Cyborg Franky',
                         ['mechanic', 'shipwrite', 'kind-hearted'], 
                         ['childish', 'aloofness'], 
                         'sprites/franky-trans.png', 
-                        `He's a cyborg`
+                        `He's a cyborg`,
                         [
                             {class: 'sprite-one', position: '-111px -376px', height: '73px', width: '103px'},
                             {class: 'sprite-two', position: '-219px -390px', height: '59px', width: '96px'},
@@ -119,7 +143,7 @@ const brook = new Theme('Soul King Brook',
                         ['singing', 'entertainment', 'swordplay'], 
                         ['0 grace', 'scares easily'], 
                         'sprites/brook-trans.png', 
-                        'Shikomizue and Music'
+                        'Shikomizue and Music',
                         [
                             {class: 'sprite-one', position: '-3px -112px', height: '79px', width: '63px'},
                             {class: 'sprite-two', position: '-152px -120px', height: '71px', width: '121px'},
