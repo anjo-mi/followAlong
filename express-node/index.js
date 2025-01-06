15a29934afaba50b7159e8f010e6874f21e51336
kyle <nojaim.k@gmail.com>
2025-01-06 16:48:57 -0500

    save

--- express-node/index.js
+++ express-node/index.js
@@ -25,33 +25,53 @@ let notes = [
     }
   ]
 
-// const app = http.createServer((req, res) => {
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify(notes));
-// });
-
-app.get('/', (req,res) => {
-    res.send('<h1>Hello World</h1>')
-});
-
-app.get('/api/notes/:id', (req,res) => {
-    const id = req.params.id;
-    const note = notes.find(note => note.id === id);
-    if (note) {
+  
+  // const app = http.createServer((req, res) => {
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(notes));
+    // });
+    
+    app.get('/', (req,res) => {
+      res.send('<h1>Hello World</h1>')
+    });
+    
+    app.get('/api/notes/:id', (req,res) => {
+      const id = req.params.id;
+      const note = notes.find(note => note.id === id);
+      if (note) {
         res.json(note);
-    } else {
+      } else {
         res.status(404).end();
-    }
-})
+      }
+    })
+    
+    app.get('/api/notes', (req,res) => {
+      res.json(notes);
+    });
+    
+  const generateID = () => {
+    const maxID = notes.length > 0 
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0;
+    return String(maxID + 1);
+  };
 
-app.get('/api/notes', (req,res) => {
-    res.json(notes);
-});
+    app.post('/api/notes', (req, res) => {
+      const body = request.body;
 
-app.post('/api/notes', (req, res) => {
-    const note = req.body;
-    console.log(note);
-    res.json(note);
+      if (!body.content){
+        return response.status(400).json({error: 'content missing'});
+      }
+
+      const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateID()
+      };
+
+      notes = notes.concat(note);
+
+      res.json(note);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
